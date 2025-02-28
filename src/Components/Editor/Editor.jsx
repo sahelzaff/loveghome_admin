7fb38fe4966b22db9f62feba46aed6386c43c055
sahelzaff <sahelzaffar2@gmail.com>
2025-02-28 16:56:12 +0530

    changes tinymce api key to sahelzaffar1@gmail.com

--- src/Components/Editor/Editor.jsx
+++ src/Components/Editor/Editor.jsx
@@ -4,7 +4,7 @@ import { Editor } from '@tinymce/tinymce-react';
 export default function App() {
   return (
     <Editor
-      apiKey='6i4ekg4ip08hajw6dak9q9oh36eao9k836fww7kmte9glc7a'
+      apiKey='csppoe5r4gng080o0udrfh18048w48yhz07wp2eyf5gila7u'
       init={{
         plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
         toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
@@ -16,7 +16,7 @@ export default function App() {
         ],
         ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
       }}
-      initialValue="Welcome to TinyMCE!"
+      initialValue="Please write your content here..."
     />
   );
 }
